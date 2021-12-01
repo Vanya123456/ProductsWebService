2da15f916b12596e6e82642b9b3a3e52a34dcdb6
Ivan <ivan.buyukli21@gmail.com>
2021-12-01 20:44:17 +0100

    Initial commit

---
+++ src/main/java/com/buyukli/ivan/productswebservice/ProductsWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buyukli.ivan.productswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buyukli/ivan/productswebservice/ProductsWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyukli.ivan.productswebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
